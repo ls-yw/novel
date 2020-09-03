5a63440fa590929084db84bc17f1bfe5bf909fda
yls <yelinsen@mayitanlu.cn>
2020-09-03 17:54:43 +0800

    test

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
 define('APP_NAME', 'novel');
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Request-Headers: *");
+header("Access-Control-Allow-Origin: *");
 
 require_once '../vendor/woodlsy/phalcon/public/index.php';
